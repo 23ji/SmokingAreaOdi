e700d20cccda1ba6d2d0c765f8f8577857603128
23ji <isj4194@naver.com>
2025-01-17 16:16:08 +0900

    git ignore 업데이트

--- SmokeAreaOdi/MarkerManager.swift
+++ SmokeAreaOdi/MarkerManager.swift
@@ -2,35 +2,7 @@
 //  MarkerManager.swift
 //  SmokeAreaOdi
 //
-//  Created by 이상지 on 1/10/25.
+//  Created by 이상지 on 1/17/25.
 //
 
-import NMapsMap
-
-class MarkerManager {
-    // MARK: - Properties
-    private weak var mapView: NMFMapView?  // 맵뷰 참조
-    private var currentMarker: NMFMarker?
-    
-    // MARK: - Initializer
-    init(mapView: NMFMapView) {
-        self.mapView = mapView
-    }
-    
-    // MARK: - Methods
-    func addMarker(at center: NMGLatLng) {
-        // 마커 생성 및 추가
-        if currentMarker == nil {
-            currentMarker = NMFMarker()
-        }
-        currentMarker?.position = center
-        currentMarker?.mapView = mapView
-        currentMarker?.captionText = "새로운 마커"
-    }
-    
-    func removeMarker() {
-        // 마커 제거
-        currentMarker?.mapView = nil
-        currentMarker = nil
-    }
-}
+import Foundation
